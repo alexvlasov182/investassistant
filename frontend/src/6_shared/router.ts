2a86066814728991cc0561b85dce0c35c3f0f0e6
Oleksandr Vlasov <drumlife182@gmail.com>
2025-10-10 19:41:15 +0200

    fix: changed route

--- frontend/src/6_shared/router.ts
+++ frontend/src/6_shared/router.ts
@@ -3,5 +3,5 @@ import { createRouter } from '@nanostores/router';
 export const $router = createRouter({
   home: '/',
   investments: '/investments',
-  investment: '/investment/:investmentId',
+  investment: '/investments/:investmentId',
 });
